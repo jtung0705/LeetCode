ccc14fd7557e07fed15d653832313dc21ba49400
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-04-13 18:47:51 -0400

    Time: 0 ms (100.00%), Space: 6.4 MB (11.45%) - LeetHub

---
+++ 0020-valid-parentheses/0020-valid-parentheses.cpp
@@ -0,0 +1,33 @@
+class Solution {
+public:
+    bool isValid(string str) {
+ /*       if (str.length() % 2 == 1 ){
+            return false;
+        }
+        */
+        
+        stack<char> parenthesis;
+        for (char s : str) {
+            if (s == '(' || s == '{'|| s == '['){
+                 parenthesis.push(s);
+            }
+            else if ( !parenthesis.empty() && s == '}' && parenthesis.top() == '{' ){
+                parenthesis.pop();
+            }
+            else if ( !parenthesis.empty() && s == ')' && parenthesis.top() == '(' ){
+                parenthesis.pop();
+            }
+            else if ( !parenthesis.empty() && s == ']' && parenthesis.top() == '[' ){
+                parenthesis.pop();
+            }
+            else{ 
+                // this is if parenthesis is empty but it is trying to push right parenthesis, as in first char
+                // or the string is not parenthesis only
+            return false;
+            }
+        
+        }
+        
+    return parenthesis.empty();
+    }
+};
