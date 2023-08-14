3c0335449cea0a1d72ac0f985c2daf4cc0859468
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-08-14 17:27:45 -0400

    Time: 315 ms (17.42%), Space: 780.6 MB (7.45%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -2,21 +2,35 @@ class Solution {
 public:
     string removeDuplicates(string s) {
         stack<char> stk; 
-        string answer = "";
+        string answer = "", ans2 = "", ans3 = "";
         for (char ch : s){
              if ( !stk.empty() and stk.top() == ch ){
+                 // cannot have stack top first, because if you do, they don't check if stack is empty
               stk.pop();
               }
              else{     
              stk.push(ch);
              }
         }
-        
+        while ( !stk.empty() ){
+            answer = stk.top() + answer; // will add to the left side of the string first, and then the rest
+            stk.pop();
+        }
+        return answer;
+        /*
          for (int i = stk.size(); i > 0; i--){
-             answer += stk.top();
+             //starts from top, goes down
+             answer += stk.top(); // have to reverse if I do this because its like
+             " " + k
+             then 
+             k + j
+             versus
+             answer = stk.top + answer; 
+             which would be k + " " 
+             then jk  
              stk.pop();
         }
         reverse( answer.begin(), answer.end() );
-        return answer;
+        */
     }
 };
