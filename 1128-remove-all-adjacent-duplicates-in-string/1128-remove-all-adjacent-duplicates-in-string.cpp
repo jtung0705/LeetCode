1b661f704d184e8bcab72239aa489e4e00ee01e8
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-08-14 16:34:56 -0400

    Time: 60 ms (20%), Space: 11.6 MB (14.02%) - LeetHub

---
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    string removeDuplicates(string s) {
+        stack<char> stk; 
+        string answer = "";
+        for (char ch : s){
+            cout << "hi"; 
+             if ( !stk.empty() and stk.top() == ch ){
+              stk.pop();
+              }
+             else{     
+             stk.push(ch);
+             }
+        }
+        
+         for (int i = stk.size(); i > 0; i--){
+             answer += stk.top();
+             stk.pop();
+        }
+        reverse( answer.begin(), answer.end() );
+        return answer;
+    }
+};
