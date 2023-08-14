47fabd81e81519bf7e6428b94d2fee034cf0f781
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-08-14 17:29:03 -0400

    Time: 28 ms (40.79%), Space: 11.6 MB (21.3%) - LeetHub

--- 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
+++ 1128-remove-all-adjacent-duplicates-in-string/1128-remove-all-adjacent-duplicates-in-string.cpp
@@ -12,25 +12,19 @@ public:
              stk.push(ch);
              }
         }
-        while ( !stk.empty() ){
-            answer = stk.top() + answer; // will add to the left side of the string first, and then the rest
-            stk.pop();
-        }
-        return answer;
-        /*
          for (int i = stk.size(); i > 0; i--){
              //starts from top, goes down
              answer += stk.top(); // have to reverse if I do this because its like
-             " " + k
-             then 
-             k + j
-             versus
-             answer = stk.top + answer; 
-             which would be k + " " 
-             then jk  
+            //  " " + k
+            //  then 
+            //  k + j
+            //  versus
+            //  answer = stk.top + answer; 
+            //  which would be k + " " 
+            //  then jk  
              stk.pop();
         }
         reverse( answer.begin(), answer.end() );
-        */
+     return answer;   
     }
 };
