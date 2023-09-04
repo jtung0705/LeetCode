f56d0864ae0b8bdef2ebefbb4a4ddc1afc7f7d32
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-09-04 13:55:35 -0400

    Time: 4 ms (89.35%), Space: 7.3 MB (87.92%) - LeetHub

--- 0242-valid-anagram/0242-valid-anagram.cpp
+++ 0242-valid-anagram/0242-valid-anagram.cpp
@@ -1,8 +1,18 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-        sort(s.begin(), s.end());
-        sort(t.begin(), t.end());
-    return s == t;
+        int histogram[26] = {0};
+        for (char c : s){
+              histogram[c % 'a']++;
+        }
+        for (char ch : t){
+            histogram[ch % 'a']--;
+        }
+        for (int i = 0; i < 26; i++){
+            if(histogram[i] != 0){
+                return false;
+            }
+        }
+        return true;
     }
 };
