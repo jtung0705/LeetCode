13d96f070a43459204ba2a8fac7cb57ad173b8fa
Jeffery Tung <120071290+jtung0705@users.noreply.github.com>
2023-09-12 19:41:08 -0400

    Time: 26 ms (94.70%), Space: 20.3 MB (77.88%) - LeetHub

--- 0141-linked-list-cycle/0141-linked-list-cycle.py
+++ 0141-linked-list-cycle/0141-linked-list-cycle.py
@@ -6,7 +6,7 @@
 
 class Solution(object):
     def hasCycle(self, head):
-        slow,fast = head, head
+        slow = head; fast = head
         while fast and fast.next:
             slow = slow.next
             fast = fast.next.next
